d87a270ee4a40e3c914ebf6c7705f2203842b624
Juan Camilo <juacostar@unal.edu.co>
2022-10-19 21:19:52 -0500

    first steps login

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/src/presentation/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  runApp(LoginPage());
 }
 
 class MyApp extends StatelessWidget {
